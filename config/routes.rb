cf34c33c00df5b890c023e17b59fd2ebb6206f34
vagrant <vagrant@localhost.localdomain>
2020-06-01 03:59:46 +0000

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root :to => 'home#top'
   resources :books
+  root to: 'home#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
